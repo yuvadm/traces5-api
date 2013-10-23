eee2539af99618ac335169276ea10c9adcc4367e
Yuval Adam <yuv.adm@gmail.com>
2013-10-23 14:55:15 +0300

    Add database boilerplate

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask.ext import restful
+from flask.ext.sqlalchemy import SQLAlchemy
+
 from os import environ
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+
+db = SQLAlchemy(app)
 api = restful.Api(app)
 
 class HelloWorld(restful.Resource):
