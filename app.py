6f0fa262c393f07a96ab6efbf0acf22b4da67bb9
Yuval Adam <yuv.adm@gmail.com>
2013-10-23 21:36:45 +0300

    Add initial Trace model

--- app.py
+++ app.py
@@ -5,14 +5,29 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from os import environ
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL', 'postgres://localhost:5432/traces')
 
 db = SQLAlchemy(app)
 api = restful.Api(app)
 
+
+class Trace(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    page = db.Column(db.String(100))
+    trace = db.Column(db.Text())
+
+    def __init__(self, page, trace):
+        self.page = page
+        self.trace = trace
+
+    def __repr__(self):
+        return '{} {}'.format(self.id, self.page)
+
+
 class HelloWorld(restful.Resource):
     def get(self):
-        return {'hello': 'world'}
+        traces = Trace.query.all()
+        return traces[0].trace
 
 api.add_resource(HelloWorld, '/')
 
